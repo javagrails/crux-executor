5e69e4f94cd06e0051949f07af48b176cd291fff
mmsaleh <salman1m22@gmail.com>
2023-08-28 22:44:18 +0600

    DynamicArray property added

--- src/main/java/crux/executor/app/neetcode/adsb/arrays/practice/DynamicArray.java
+++ src/main/java/crux/executor/app/neetcode/adsb/arrays/practice/DynamicArray.java
@@ -2,9 +2,13 @@ package crux.executor.app.neetcode.adsb.arrays.practice;
 
 public class DynamicArray {
 
+  int capacity;
+  int length;
+  int[] array;
+
   // get
   // insertEnd
   // increaseSize
   // more on
-  
+
 }
