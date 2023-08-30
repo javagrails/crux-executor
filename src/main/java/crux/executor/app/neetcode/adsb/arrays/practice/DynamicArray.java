78341ac9d0fac8043a8bd4bd66e29e3e23d7142e
mmsaleh <salman1m22@gmail.com>
2023-08-30 17:50:59 +0600

    not started yet

--- src/main/java/crux/executor/app/neetcode/adsb/arrays/practice/DynamicArray.java
+++ src/main/java/crux/executor/app/neetcode/adsb/arrays/practice/DynamicArray.java
@@ -2,8 +2,8 @@ package crux.executor.app.neetcode.adsb.arrays.practice;
 
 public class DynamicArray {
 
-  int capacity;
-  int length;
+  int capacity; // number of items
+  int length;  // length of the array
   int[] array;
 
   // get
