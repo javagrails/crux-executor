9ef77e579b5cf586b067ed4487d73a411ca2fca4
mmsaleh <salman1m22@gmail.com>
2023-09-03 22:36:22 +0600

    cons removed

--- src/main/java/crux/executor/app/neetcode/adsb/arrays/practice/DynamicArray.java
+++ src/main/java/crux/executor/app/neetcode/adsb/arrays/practice/DynamicArray.java
@@ -10,12 +10,5 @@ public class DynamicArray {
   // insertEnd
   // increaseSize
   // more on
-
-  public DynamicArray() {}
-
-  public DynamicArray(int capacity, int length, int[] array) {
-    this.capacity = capacity;
-    this.length = length;
-    this.array = array;
-  }
+  
 }
