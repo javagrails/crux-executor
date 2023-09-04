ff7c5850ca5c20b0bd443daa4c40fc7fc467daf4
mmsaleh <salman1m22@gmail.com>
2023-09-04 17:39:12 +0600

    cons back

--- src/main/java/crux/executor/app/neetcode/adsb/arrays/practice/DynamicArray.java
+++ src/main/java/crux/executor/app/neetcode/adsb/arrays/practice/DynamicArray.java
@@ -10,5 +10,12 @@ public class DynamicArray {
   // insertEnd
   // increaseSize
   // more on
-  
+
+  public DynamicArray() {}
+
+  public DynamicArray(int capacity, int length, int[] array) {
+    this.capacity = capacity;
+    this.length = length;
+    this.array = array;
+  }
 }
